78b87350719bb290975bde6e5c5033b05ea0cfd6
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2022-10-04 17:27:51 +0200

    Updated documentation module

--- modules_pydocs/tree_type.py
+++ modules_pydocs/tree_type.py
@@ -1,10 +1,6 @@
 r"""
 The different types of trees.
 
-- ``bistar`` Bi-star trees
-
-	These trees are made of two star trees joined by an edge at their centers.
-
 - ``caterpillar`` Caterpillar trees
 
 	These are the trees such that a linear tree is produced when its leaves are removed [2].
@@ -19,11 +15,21 @@ The different types of trees.
 
 - ``spider`` Spider trees
 
-	A spider tree has a unique vertex of degree greater than or equal to 3. The other vertices have degree 2 or 1 [1].
+	A spider tree has a unique vertex of degree greater than or equal to 3. The other vertices have degree 2 or 1 [1], [3].
+
+- ``bispider`` Bi-spider trees.
+
+	A bi-spider tree is a tree in which there are at most two vertices of degree 3 or larger; the other vertices have degree 2 or 1.
+	
+	It can also be seen as the connection of two independent trees :math:`T_1` and :math:`T_2` where each tree is either a spider tree or a linear tree. If one of :math:`T_1` or :math:`T_2` is a spider tree, the path must be incident to the spider's hub vertex.
 
 - ``star`` Star trees
 
 	Also star graphs, trees where all vertices but one have degree 1.
+
+- ``bistar`` Bi-star trees
+
+	These trees are made of two star trees joined by an edge at their centers.
 
 - ``unknown``
 
@@ -35,5 +41,7 @@ References
 .. [1] Bennett, Patrick and English, Sean and Talanda-Fisher, Maria. Weighted turán problems with applications. *Discrete Mathematics*, 342(8):2165–2172, 2019. https://doi.org/10.1016/j.disc.2019.04.007.
 
 .. [2] Frank Harary and Allen J. Schwenk. The number of caterpillars. *Discrete Mathematics*, 6:359–365, 1973. https://doi.org/10.1016/0012-365X(73)90067-8.
+
+.. [3] Spider Graph -- Wolfram Math World. https://mathworld.wolfram.com/SpiderGraph.html. Accessed: 2022-10-04.
 
 """
