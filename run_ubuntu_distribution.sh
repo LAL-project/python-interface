f13c04fa5d5adbf8952b6316d435b2cd86e2d798
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2023-01-11 12:49:30 +0100

    Minor changes

--- run_ubuntu_distribution.sh
+++ run_ubuntu_distribution.sh
@@ -9,6 +9,7 @@ PYTHON_HEADERS="/usr/include/python3.8"
 PYTHON_LIBRARY="/usr/lib/x86_64-linux-gnu/"
 
 ./compile.sh \
+	--environment=distribution \
 	--lal-headers="$LAL_HEADERS" \
 	--lal-libs="$LAL_LIBRARY" \
 	--gmp-headers="" \
@@ -20,6 +21,7 @@ PYTHON_LIBRARY="/usr/lib/x86_64-linux-gnu/"
 	--build=release
 
 ./compile.sh \
+	--environment=distribution \
 	--lal-headers="$LAL_HEADERS" \
 	--lal-libs="$LAL_LIBRARY" \
 	--gmp-headers="" \
@@ -36,6 +38,7 @@ elif [ "$1" == "miniconda" ]; then
 miniconda_dir="/home/$USER/miniconda3"
 
 ./compile.sh \
+	--environment=distribution \
 	--lal-headers="$LAL_HEADERS" \
 	--lal-libs="$LAL_LIBRARY" \
 	--gmp-headers="" \
@@ -48,6 +51,7 @@ miniconda_dir="/home/$USER/miniconda3"
 	--anaconda
 
 ./compile.sh \
+	--environment=distribution \
 	--lal-headers="$LAL_HEADERS" \
 	--lal-libs="$LAL_LIBRARY" \
 	--gmp-headers="" \
@@ -65,6 +69,7 @@ elif [ "$1" == "anaconda" ]; then
 anaconda_dir="/home/$USER/anaconda3"
 
 ./compile.sh \
+	--environment=distribution \
 	--lal-headers="$LAL_HEADERS" \
 	--lal-libs="$LAL_LIBRARY" \
 	--gmp-headers="" \
@@ -77,6 +82,7 @@ anaconda_dir="/home/$USER/anaconda3"
 	--anaconda
 
 ./compile.sh \
+	--environment=distribution \
 	--lal-headers="$LAL_HEADERS" \
 	--lal-libs="$LAL_LIBRARY" \
 	--gmp-headers="" \
