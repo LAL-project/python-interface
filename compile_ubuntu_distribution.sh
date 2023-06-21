284037dadd2a5608b72f538a9b14cf28246296d7
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2023-06-21 11:15:05 +0200

    Use correct parameters and environment variables

--- compile_ubuntu_distribution.sh
+++ compile_ubuntu_distribution.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-LAL_HEADERS="/home/$USER/Documents/projects/LAL-dev/23.01/c++-libs/include"
-LAL_LIBRARY="/home/$USER/Documents/projects/LAL-dev/23.01/c++-libs/lib"
+LAL_HEADERS="$HOME/Documents/projects/LAL-dev/23.01/c++-libs/include"
+LAL_LIBRARY="$HOME/Documents/projects/LAL-dev/23.01/c++-libs/lib"
 
 if [ -z $1 ]; then
 
