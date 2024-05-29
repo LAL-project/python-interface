8daa6410d093af3f8c64dd98d0a0c795f798f72d
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2024-05-29 13:19:13 +0200

    Updated compilation and installation scripts

--- windows_compile_development.sh
+++ windows_compile_development.sh
@@ -6,56 +6,27 @@ LAL_LIBRARY_DEBUG="../linear-arrangement-library/lal-debug"
 
 if [ -z $1 ]; then
 
-PYTHON_HEADERS="C:/msys64/mingw64/include/python3.10"
+PYTHON_HEADERS="C:/msys64/mingw64/include/python3.11"
 PYTHON_LIBRARY=""
 
 ./compile.sh \
+	--environment=development \
 	--lal-headers="$LAL_HEADERS" \
 	--lal-libs="$LAL_LIBRARY" \
 	--gmp-headers="" \
 	--gmp-libs="" \
 	--python-headers="$PYTHON_HEADERS" \
 	--python-libs="$PYTHON_LIBRARY" \
-	--python-major="3" \
-	--python-minor="10" \
 	--build=release
 
 ./compile.sh \
+	--environment=development \
 	--lal-headers="$LAL_HEADERS" \
 	--lal-libs="$LAL_LIBRARY" \
 	--gmp-headers="" \
 	--gmp-libs="" \
 	--python-headers="$PYTHON_HEADERS" \
 	--python-libs="$PYTHON_LIBRARY" \
-	--python-major="3" \
-	--python-minor="10" \
-	--build=debug
-
-# for miniconda users only
-elif [ "$1" == "miniconda" ]; then
-
-miniconda_dir="C:/Users/$USER/miniconda3"
-
-./compile.sh \
-	--lal-headers="$LAL_HEADERS" \
-	--lal-libs="$LAL_LIBRARY" \
-	--gmp-headers="" \
-	--gmp-libs="" \
-	--python-headers="$miniconda_dir/include/python3.9" \
-	--python-libs="$miniconda_dir/lib" \
-	--python-major="3" \
-	--python-minor="9" \
-	--build=release
-
-./compile.sh \
-	--lal-headers="$LAL_HEADERS" \
-	--lal-libs="$LAL_LIBRARY" \
-	--gmp-headers="" \
-	--gmp-libs="" \
-	--python-headers="$miniconda_dir/include/python3.9" \
-	--python-libs="$miniconda_dir/lib" \
-	--python-major="3" \
-	--python-minor="9" \
 	--build=debug
 
 # for anaconda users only
@@ -64,25 +35,23 @@ elif [ "$1" == "anaconda" ]; then
 anaconda_dir="C:/Users/$USER/anaconda3"
 
 ./compile.sh \
+	--environment=development \
 	--lal-headers="$LAL_HEADERS" \
 	--lal-libs="$LAL_LIBRARY" \
 	--gmp-headers="" \
 	--gmp-libs="" \
 	--python-headers="$anaconda_dir/include/" \
 	--python-libs="$anaconda_dir" \
-	--python-major="3" \
-	--python-minor="9" \
 	--build=release
 
 ./compile.sh \
+	--environment=development \
 	--lal-headers="$LAL_HEADERS" \
 	--lal-libs="$LAL_LIBRARY" \
 	--gmp-headers="" \
 	--gmp-libs="" \
 	--python-headers="$anaconda_dir/include/" \
 	--python-libs="$anaconda_dir" \
-	--python-major="3" \
-	--python-minor="9" \
 	--build=debug
 
 fi
--- windows_install.sh
+++ windows_install.sh
@@ -3,34 +3,20 @@
 if [ -z $1 ]; then
 
 ./compile.sh \
-	--destination-directory="C:/programming/msys64/mingw64/lib/python3.10/site-packages" \
+	--destination-directory="C:/msys64/mingw64/lib/python3.10/site-packages" \
 	--build=release \
 	--install
 
 ./compile.sh \
-	--destination-directory="C:/programming/msys64/mingw64/lib/python3.10/site-packages" \
-	--build=debug \
-	--install
-
-# for miniconda users only
-elif [ "$1" == "miniconda" ]; then
-
-miniconda_dir="C:/Users/$USER/miniconda3/Lib/site-packages"
-
-./compile.sh \
-	--destination-directory="$miniconda_dir" \
-	--build=release \
-	--install
-
-./compile.sh \
-	--destination-directory="$miniconda_dir" \
+	--destination-directory="C:/msys64/mingw64/lib/python3.10/site-packages" \
 	--build=debug \
 	--install
 
 # for anaconda users only
 elif [ "$1" == "anaconda" ]; then
 
-anaconda_dir="C:/Users/$USER/anaconda3/Lib/site-packages"
+anaconda_dir="C:/Users/$USER/anaconda3"
+anaconda_package_dir="$anaconda_dir/Lib/site-packages"
 
 ./compile.sh \
 	--destination-directory="$anaconda_dir" \
@@ -41,20 +27,48 @@ anaconda_dir="C:/Users/$USER/anaconda3/Lib/site-packages"
 	--destination-directory="$anaconda_dir" \
 	--build=debug \
 	--install
+
+lal_dist_dir="C:/Users/$USER/Desktop/LAL-DISTRIBUTION"
+lal_cpp_dir="$lal_dist_dir/latest/windows10-c++-libs-msys64"
+
+# lal dlls
+cp $lal_cpp_dir/bin/liblal.dll $anaconda_package_dir/lal
+cp $lal_cpp_dir/bin/liblaloptimized.dll $anaconda_package_dir/laloptimized
+
+# system dlls
+cp $lal_dist_dir/libgcc_s_seh-1.dll $anaconda_dir
+cp $lal_dist_dir/libgmp-10.dll $anaconda_dir
+cp $lal_dist_dir/libgomp-1.dll $anaconda_dir
+cp $lal_dist_dir/libstdc++-6.dll $anaconda_dir
+cp $lal_dist_dir/libwinpthread-1.dll $anaconda_dir
 
 # for developers only
 elif [ "$1" == "package" ]; then
 
-destdir="C:/Users/$USER/Desktop/LAL-DISTRIBUTION/latest/windows10-python3.9-libs-anaconda"
+dest_dir="C:/Users/$USER/Desktop/LAL-DISTRIBUTION/latest/windows10-python3-libs-anaconda"
 
 ./compile.sh \
-	--destination-directory="$destdir" \
+	--destination-directory="$dest_dir" \
 	--build=release \
 	--install
 
 ./compile.sh \
-	--destination-directory="$destdir" \
+	--destination-directory="$dest_dir" \
 	--build=debug \
 	--install
+
+lal_dist_dir="C:/Users/$USER/Desktop/LAL-DISTRIBUTION"
+lal_cpp_dir="$lal_dist_dir/latest/windows10-c++-libs-msys64"
+
+# lal dlls
+cp $lal_cpp_dir/bin/liblal.dll $dest_dir/lal
+cp $lal_cpp_dir/bin/liblaloptimized.dll $dest_dir/laloptimized
+
+# system dlls
+cp $lal_dist_dir/libgcc_s_seh-1.dll $dest_dir
+cp $lal_dist_dir/libgmp-10.dll $dest_dir
+cp $lal_dist_dir/libgomp-1.dll $dest_dir
+cp $lal_dist_dir/libstdc++-6.dll $dest_dir
+cp $lal_dist_dir/libwinpthread-1.dll $dest_dir
 
 fi
